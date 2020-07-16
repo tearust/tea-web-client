5616526e2ccd4139b3c36d9bbc9f609801ddf8e0
jacky.li <jacky.li@accenture.com>
2020-07-16 18:06:30 +0800

    update

--- src/tea/types.js
+++ src/tea/types.js
@@ -1,28 +1,27 @@
 const types = {
-  Weight: "u32",
-  Address: "AccountId",
-  TeaPubKey: "[u8; 32]",
-  Url: "Bytes",
-  RefNum: "H256",
-  Result: "Bytes",
-  Node: {
-        "teaId": "TeaPubKey",
-        "ephemeralId": "TeaPubKey",
-        "profileCid": "Bytes",
-        "urls": "Vec<Url>"
-  },
-  Model: {
-        "account": "AccountId",
-        "payment": "u32",
-        "cid": "Bytes"
-  },
-  Task: {
-        "refNum": "RefNum",
-        "delegateTeaId": "TeaPubKey",
-        "modelCid": "Bytes",
-        "bodyCid": "Bytes",
-        "payment": "Balance"
-  }
+      Address: "AccountId",
+      TeaPubKey: "[u8; 32]",
+      Url: "Bytes",
+      RefNum: "H256",
+      Result: "Bytes",
+      Node: {
+            "teaId": "TeaPubKey",
+            "ephemeralId": "TeaPubKey",
+            "profileCid": "Bytes",
+            "urls": "Vec<Url>"
+      },
+      Model: {
+            "account": "AccountId",
+            "payment": "u32",
+            "cid": "Bytes"
+      },
+      Task: {
+            "refNum": "RefNum",
+            "delegateTeaId": "TeaPubKey",
+            "modelCid": "Bytes",
+            "bodyCid": "Bytes",
+            "payment": "Balance"
+      }
 }
 
 module.exports = types;
