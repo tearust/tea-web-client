79e89d029b0a6cdbc9971ca09239881f2f19a2e7
jacky.li <jacky.li@accenture.com>
2020-09-15 13:02:25 +0800

    update

--- src/deploy_data_views/DeployResult.vue
+++ src/deploy_data_views/DeployResult.vue
@@ -4,14 +4,26 @@
 
   <h4>Session ID: {{session_id}}</h4>
   <h4>Deployment ID: {{deployment_id}}</h4>
+
+  <div style="display:flex;">
+    <el-button type="primary" style="width:150px;"  
+      @click="find_provider()">Find provider</el-button>
+
+    <el-button style="width:100px; margin-left: 50px;" 
+    type="primary" 
+    @click="refresh()">Refresh</el-button>
+  </div>
+
   <h4 v-if="deployment_id">Peers List <font v-if="loading" style="font-size:14px;color:#9c9c9c;margin-left: 15px;">querying...</font></h4>
-  <p v-if="peers_list">{{peers_list}}</p>
+  <h5 v-for="(addr, i) in peers_list" :key="i">{{addr}}</h5>
+  
 </div>
 </template>
 <script>
 import DeployHelper from '../workflow/DeployHelper';
 import DeployDataTaskStep from './DeployDataTaskStep';
 import _ from 'lodash';
+import PubSub from 'pubsub-js';
 export default {
   components: {
     DeployDataTaskStep
@@ -32,12 +44,20 @@ export default {
       }
 
       if(step === 3){
-        this.peers_list = JSON.stringify(res.peers);
+        this.peers_list = res.peers;
       }
       if(step === 4){
         this.loading = false;
       }
     });
+  },
+  methods: {
+    async find_provider(){
+      PubSub.publish('test-btn-a', {});
+    },
+    async refresh(){
+      PubSub.publish('test-btn-b', {});
+    }
   }
 }
 </script>
--- src/workflow/DeployHelper.js
+++ src/workflow/DeployHelper.js
@@ -3,15 +3,14 @@ import _ from 'lodash';
 import utils from '../tea/utils';
 import Log from '../shared/utility/Log';
 
+import PubSub from 'pubsub-js';
+
 const log = Log.create('Deploy Helper');
 
 export default class {
   // static 
   static queryTaskResult = async (session_id, cb)=>{
     let deployment_id = null;
-
-    let n = 1;
-    const MAX = 10;
 
     const res_1 = await http.query_deployment_id_by_session_id(session_id);
     log.d('query_deployment_id_by_session_id response', res_1);
@@ -20,37 +19,61 @@ export default class {
       deployment_id,
     });
 
-    const res_2 = await http.start_query_pinners_by_deployment_id(deployment_id);
-    log.d('start_query_pinners_by_deployment_id response', res_2);
+    const fn_2 = async ()=>{
+      const res_2 = await http.start_query_pinners_by_deployment_id(deployment_id);
+      log.d('start_query_pinners_by_deployment_id response', res_2);
 
-    cb(2, {
+      cb(2, {
 
-    });
+      });
+    };
 
     let peers_list = [
-      'Jacky mock peer_id',
+      // 'Jacky mock peer_id',
     ];
     const loop = async ()=>{
       const res_3 = await http.query_pinners_by_deployment_id(deployment_id);
       log.d('query_pinners_by_deployment_id response', res_3);
 
       const list = res_3.peers;
-      peers_list = _.concat(list, peers_list)
+      // peers_list = _.uniq(_.concat(list, peers_list));
+      peers_list = _.uniq(list);
 
       cb(3, {
         peers: peers_list
       });
 
-      if(n > MAX){
-        cb(4, {});
-        return false;
-      }
-      n++;
-      _.delay(async ()=>{
-        await loop();
-      }, 5000);
+      // if(n > MAX){
+      //   cb(4, {});
+      //   return false;
+      // }
+      // n++;
+      // _.delay(async ()=>{
+      //   await loop();
+      // }, 5000);
     };
 
-    await loop();
+    const fn_3 = async ()=>{
+      await loop();
+    };
+
+    const key1 = 'test-btn-a';
+    const key2 = 'test-btn-b';
+    PubSub.unsubscribe(key1);
+    PubSub.subscribe(key1, async ()=>{
+      await fn_2();
+    });
+
+    PubSub.unsubscribe(key2);
+    PubSub.subscribe(key2, async ()=>{
+      await fn_3();
+    });
+    
+    
+    await fn_2();
+    // await loop();
+    _.delay(async ()=>{
+      await fn_3();
+    }, 5000);
   }
 }
