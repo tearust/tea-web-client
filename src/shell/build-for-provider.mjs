8878ea7fec796468c2a5c671848a2022b81de30a
jacky.li <jacky.li@accenture.com>
2020-10-31 10:24:28 +0800

    update

--- src/shell/build-for-provider.mjs
+++ src/shell/build-for-provider.mjs
@@ -1,42 +1,6 @@
 import * as help from './help';
 import _ from 'lodash';
 
-const ROOT_PATH="../builds";
-const PATH_1=`${ROOT_PATH}/debug`;
-const LIST = [
-  {
-    name: 'libtea_tensorflow_provider',
-    url: `${PATH_1}/libtea_tensorflow_provider.so`
-  },
-  {
-    name: 'libtea_tpm_provider',
-    url: `${PATH_1}/libtea_tpm_provider.so`
-  },
-  {
-    name: 'libwascc_httpsrv',
-    url: `${PATH_1}/libwascc_httpsrv.so`
-  },
-  {
-    name: 'libtea_keyvalue_provider',
-    url: `${PATH_1}/libtea_keyvalue_provider.so`
-  },
-  {
-    name: 'libtea_nats_provider',
-    url: `${PATH_1}/libtea_nats_provider.so`
-  },
-  {
-    name: 'libtea_env_provider',
-    url: `${PATH_1}/libtea_env_provider.so`
-  },
-  {
-    name: 'libtea_ipfs_provider',
-    url: `${PATH_1}/libtea_ipfs_provider.so`
-  },
-  {
-    name: 'libwascc_log',
-    url: `${PATH_1}/libwascc_log.so`
-  }
-];
 
 
 const Deploy = class {
